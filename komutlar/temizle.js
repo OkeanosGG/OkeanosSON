e5896b92d33f050933daeace4296fc95491ef468
OkeanosGG <48868393+OkeanosGG@users.noreply.github.com>
2019-03-29 22:59:54 +0300

    Update temizle.js

--- komutlar/temizle.js
+++ komutlar/temizle.js
@@ -1,21 +1,23 @@
 const Discord = require('discord.js');
-exports.run = function(client, message, args) {
-if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Bu Komutu Kullanmak İçin İzniniz Yok!");
-if(!args[0]) return message.channel.send("<:hayirr:495950887898841089> Lütfen Silinicek Mesaj Miktarını Yazın.!");
-message.channel.bulkDelete(args[0]).then(() => {
-  message.channel.send(`<:evett:495950668725747733> ${args[0]} Adet Mesajı Sildim.`)
+
+
+exports.run = function(client, message) {
+message.channel.bulkDelete(20);
+message.channel.send("20 mesaj sildim").then(msg => {
+	msg.delete(5000)
 })
-}
+
+};
 
 exports.conf = {
-  enabled: true,
-  guildOnly: true,
-  aliases: ['temizle'],
-  permLevel: 2
+  enabled: true, 
+  guildOnly: false, 
+  aliases: [],
+  permLevel: 3 
 };
 
 exports.help = {
-  name: 'temizle',
-  description: 'Belirlenen miktarda mesajı siler.',
-  usage: 'temizle <silinicek mesaj sayısı>'
+  name: 'temizle', 
+  description: 'Belirtilen miktarda mesaj siler',
+  usage: 'temizle <miktar>'
 };
